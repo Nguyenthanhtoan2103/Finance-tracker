e3bdf5eae4c2281ef2c936babc6b430de7fbb22f
Nguyenthanhtoan2103 <toanntse160451@fpt.edu.vn>
2025-09-22 22:39:14 +0700

    update url

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ app.use('/api/budgets', require('./routes/budget'));
 // --- Socket.IO setup ---
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://www.ftracker.site/"],
+    origin: "*", 
     methods: ["GET", "POST"]
   }
 });
--- backend/swagger.js
+++ backend/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:80",
+        url: "http://www.ftracker.site",
       },
     ],
     components: {
